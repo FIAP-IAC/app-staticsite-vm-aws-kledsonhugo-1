b0f2931f9c554564a4e25676a6009d57ca5136ae
Kledson Basso <kledsonhugo@gmail.com>
2024-08-11 23:06:29 -0300

    Trigger Pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 5.62"
     }
   }
-  backend "aws" {
+  backend "s3" {
     bucket         = "tf-s3-kledson-001"
     key            = "terraform.tfstate"
     dynamodb_table = "tf-dynamo-kledson-001"
